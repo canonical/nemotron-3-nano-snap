7614e506dd3e071a7943ca9cc2dc988315f39af4
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-01-22 11:58:01 +0100

    debug: manually remove lfs objects

--- .github/workflows/init-build.sh
+++ .github/workflows/init-build.sh
@@ -4,7 +4,9 @@ set -x
 
 df -h
 # Remove LFS object copies to reduce disk usage
-git lfs prune --force
-git gc --prune=now
+#git lfs prune --force
+#git gc --prune=now
+
+rm -r .git/lfs/objects
 
 df -h
