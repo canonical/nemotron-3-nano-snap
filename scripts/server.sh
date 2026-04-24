059100ced1fa924aef12bea77140f3832a3675c1
JP Meijers <jp.meijers@canonical.com>
2026-04-24 15:00:01 +0200

    Add config plugs and update llama.cpp-build source

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
+# Export the configuration for content sharing
+# This must be done each time the server is started to expose the actual configuration
+$SNAP/bin/export-shared-configs.sh
+
 engine="$(modelctl show-engine --format=json | jq -r .name)"
 exec modelctl run --wait-for-components -- "$SNAP/engines/$engine/server" "$@"
